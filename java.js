54ab714ff191cb12c024983b1e99d302b74fbd75
Wallace Vinícius <32179427+wallacevinicius@users.noreply.github.com>
2018-03-08 16:18:29 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){
+	$('.alert-danger').hide();
+	$("#loading").hide();
+    $("#creatPlot").click(function(){
+        $("#loading").fadeIn(1000);
+    });
+});
+var Upload = {
+	one: 0,
+	two: 0,
+	three: 0,
+	num: 0,
+	All: function(num){
+		var formData = new FormData($('#formUpload')[0]),
+		num = Upload.num,
+		c = $('[data-prog="'+num+'"]'),
+		d = $('[data-prog="'+num+'"] .progress-bar');
+		if(Upload.one == 0 && num == 1 || Upload.two == 0 && num == 2 || Upload.three == 0 && num == 3){
+			$.ajax({
+				type: 'POST',
+				url: '/assets/scripts/upload.php',
+				processData: false,
+				contentType: false,
+				data: formData,
+				cache: false,
+				xhr: function(){
+					var xhr = new window.XMLHttpRequest();
+					xhr.upload.addEventListener("progress", function(evt){
+						if(evt.lengthComputable){
+							var percentComplete1 = evt.loaded / evt.total;
+							d.animate({'width': Math.round(percentComplete1 * 100)+'%'});
+							console.log(Math.round(percentComplete1 * 100));
+						}
+					}, false);
+					xhr.addEventListener("progress", function(evt){
+						if(evt.lengthComputable){
+							var percentComplete1 = evt.loaded / evt.total;
+							d.animate({'width': Math.round(percentComplete1 * 100)+'%'});
+							console.log(percentComplete1);
+						}
+					}, false);
+					return xhr;
+				},
+				beforeSend: function(){
+					c.fadeIn(0);
+					d.addClass('progress-bar-animated');
+				},
+				complete: function(){
+					d.html("Upload Complete");
+					d.addClass('bg-success');
+					d.removeClass('progress-bar-animated');
+				},
+				success: function(data){
+					d.addClass('bg-success');
+					d.html("Upload Complete");
+					d.removeClass('progress-bar-animated');
+					alert(num);
+					if(num == 2){
+						if (data.error) {
+							console.info(data.error);
+
+							if ( $("#phenotypicData").val() ) {
+								$("#param").append("<option>Select</option>");
+							}
+
+							console.log( data );
+
+						} else if (data.classes) {
+
+							console.log( data );
+
+							if ( $("#phenotypicData").val() ) {
+								$("#param").html("");
+
+								$.each(data.classes, function() {
+									$("#param").append("<option>"+this+"</option>");
+								});
+							}
+						}
+					} else if(num == 3){
+						if (data.error) {
+							console.info(data.error);
+
+							if ( $("#pathwaysGMT").val() ) {
+								$("#param2").append("<option>Select</option>");
+							}
+
+							console.log( data );
+
+						} else if (data.classes) {
+
+							console.log( data );
+
+							if ( $("#pathwaysGMT").val() ) {
+								$("#param2").html("");
+
+								$.each(data.classes, function() {
+									$("#param2").append("<option>"+this+"</option>");
+								});
+							}
+						}
+					}
+
+				}
+			});
+		}
+	}
+}
+$('#formUpload').submit(function(){
+	Upload.All();
+	return false;
+});
+$('input[type=file]').change(function(){
+	var a = $(this),
+		b = a.val(),
+		c = b.substr(b.lastIndexOf('\\') + 1),
+		d = a.attr('data-id');
+	a.next().next().html(c);
+	Upload.num = d;
+	$('#formUpload').submit();
+});
